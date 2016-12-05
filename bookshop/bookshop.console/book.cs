ecae887e6e279fbabb8d534cfacfdbe007433f16
Bishan.Meghani <Bishan.Meghani@e0cfe24b-ed12-4994-be7b-b1405583678e>
2016-12-05 11:29:11 +0000

    added book class

---
+++ bookshop/bookshop.console/book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookshop.console
+{
+    class book
+    {
+        private string _title;
+        public string title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        private string _author;
+        public string author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        private string _isbn;
+        public string isbn
+        {
+            get { return _isbn; }
+            set { _isbn = value; }
+        }
+
+        private int _numberofpages;
+        public int numberofpages
+        {
+            get { return _numberofpages; }
+            set { _numberofpages = value; }
+        }
+
+        private double _price;
+        public double price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+    }
+}
