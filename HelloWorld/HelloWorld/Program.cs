0246733eb8b57cfd03c21466f58c5b5c2d855f5a
Bishan.Meghani <Bishan.Meghani@e0cfe24b-ed12-4994-be7b-b1405583678e>
2016-12-05 11:00:41 +0000

    First version of Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    // 
+    class Program
+    {
+        // Void means it doesn't give anything back.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            string userinput = Console.ReadLine();
+            Console.WriteLine(userinput);
+            Console.ReadLine();
+        }
+    }
+}
